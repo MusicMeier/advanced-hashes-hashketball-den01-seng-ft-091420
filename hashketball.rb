21cf981c57455db8c082a86cd88276d7275f53fe
Music Meier <musicm987@gmail.com>
2020-09-09 01:29:09 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -194,7 +194,7 @@ end
 def big_shoe_rebounds 
   biggest_shoe_size = 0
   game_hash.each do |home_away, keys|
-    keys[:players].each do |players|
+    keys[:players].map do |players|
       if players[:shoe] > biggest_shoe_size
         biggest_shoe_size = players[:shoe]
         binding.pry
